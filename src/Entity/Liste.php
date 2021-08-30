799613ba397947549b29ab176b23dc288877b75c
Kristofer LEDOUX <kristofer.ledoux2020@campus-eni.fr>
2021-08-30 15:07:01 +0200

    test

---
+++ src/Entity/Liste.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ListeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ListeRepository::class)
+ */
+class Liste
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $buy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBuy(): ?bool
+    {
+        return $this->buy;
+    }
+
+    public function setBuy(bool $buy): self
+    {
+        $this->buy = $buy;
+
+        return $this;
+    }
+}
